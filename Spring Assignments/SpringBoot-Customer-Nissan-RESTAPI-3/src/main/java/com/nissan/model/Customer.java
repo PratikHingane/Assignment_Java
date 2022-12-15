5316d5e26b38ac3978a894cf97c506a4fd6bac2f
HINGANE <NDH00877@Nmcorp.Nissan.Biz>
2022-12-15 09:23:02 +0530

    Customer app added

---
+++ Spring Assignments/SpringBoot-Customer-Nissan-RESTAPI-3/src/main/java/com/nissan/model/Customer.java
@@ -0,0 +1,73 @@
+package com.nissan.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tblcustomer")
+public class Customer {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer custNo;
+	private String customerName;
+	private String address;
+	
+	//Default constructor
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	//Parameterized Constructor
+	public Customer(Integer custNo, String customerName, String address) {
+		super();
+		this.custNo = custNo;
+		this.customerName = customerName;
+		this.address = address;
+	}
+
+
+	//Getters and Setters
+	public Integer getCustNo() {
+		return custNo;
+	}
+
+
+	public void setCustNo(Integer custNo) {
+		this.custNo = custNo;
+	}
+
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	// toString()
+	@Override
+	public String toString() {
+		return "Customer [custNo=" + custNo + ", customerName=" + customerName + ", address=" + address + "]";
+	}
+	
+	
+
+}
